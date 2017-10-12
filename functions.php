214463ace08b202491be4c435d3efc2f054d1e71
Kevin <mallettk@gmail.com>
2017-10-11 22:07:50 -0400

    Update css and js paths

--- functions.php
+++ functions.php
@@ -145,7 +145,7 @@ add_action( 'widgets_init', 'wsu_surgery_widgets_init' );
 function wsu_surgery_scripts() {
 	
 	/* CSS */
-	wp_enqueue_style( 'bootstrap-styles', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0', 'all' );
+	wp_enqueue_style( 'bootstrap-styles', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '4.0.0', 'all' );
     wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', false, '4.1.0' );
 	wp_enqueue_style( 'wsu_surgery-style', get_stylesheet_uri() );
 
@@ -154,8 +154,8 @@ function wsu_surgery_scripts() {
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '4.0.0', true );
 	wp_enqueue_script( 'sbd_base-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'bxslider-js', get_template_directory_uri() . '/js/jquery.bxslider.min.js', array('jquery'), '4.1.2', true );
-	wp_enqueue_script( 'site-js', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0', true );
+	wp_enqueue_script( 'bxslider-js', get_template_directory_uri() . '/assets/js/jquery.bxslider.min.js', array('jquery'), '4.1.2', true );
+	wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0', true );
 
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
